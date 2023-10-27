0b1a2958f85f8612085a3e8a3910b426cb8214fd
JosueUAB <josue.arista@uab.edu.bo>
2023-10-26 20:03:38 -0400

    examen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'contactos',component:ContactosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
